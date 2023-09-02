577a2293b6d4e5caad1818b306791c39d4bf3961
Juraj Hilje <juraj.hilje@gmail.com>
2023-09-02 08:55:38 +0200

    feat: add swift-protobuf dependency

--- IVPNClient/Models/V2Ray/V2RayCore.swift
+++ IVPNClient/Models/V2Ray/V2RayCore.swift
@@ -23,6 +23,7 @@
 
 import Foundation
 import V2Ray
+import SwiftProtobuf
 
 class V2RayCore {
     
@@ -39,13 +40,10 @@ class V2RayCore {
             core = nil
         }
         
-        let config = V2RayConfig.parse(fromJsonFile: "config")!
-        let configData = try? JSONEncoder().encode(config)
         var startError: Error? = nil
         
         do {
             let config = CoreConfig.init()
-            try config.xxX_Marshal(configData, deterministic: true)
             core = CoreNew(config, nil)
             try core?.start()
         } catch let error {
